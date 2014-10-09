75b9602464ef5114ac557c8ba8e57d383eebe003
FattoriaAsus <FattoriaAsus@FattoriaAsus-PC>
2014-10-09 10:37:35 -0300

    teste commit

--- convertBR/src/br/convert/BrConverter.java
+++ convertBR/src/br/convert/BrConverter.java
@@ -11,6 +11,7 @@ public class BrConverter {
 		ConverterCharacter converterCharacter = ConverterCharacter.getInstance(ConverterCharacter.ConvertType.CONVERT_HTML);
 	//	System.out.println(converterCharacter.convertToCode("í"));
 		
+		//teste
 		converterCharacter.addCharacters('¨');
 		
 		for (Map.Entry<String, String> m : converterCharacter.getAllCharacters().entrySet()) {
